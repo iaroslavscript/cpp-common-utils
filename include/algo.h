1d63810295b2d3da15362fede694ff1dc9fcf0ff
iaroslavscript <yaroslav.akimov@gmail.com>
2020-09-29 18:57:36 +0300

    fix OutputIt advance typo

--- include/algo.h
+++ include/algo.h
@@ -1,115 +1,47 @@
+/* MIT License
+ * 
+ * Copyright (c) 2020 Iaroslav Akimov
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 #ifndef CCU_ALGO_H
 #define CCU_ALGO_H
 
+
+#include <iterator>
+
+
 namespace ccu {
+namespace algo {
 
-template <typename T, typename R, typename U, typename K>
-struct DiffStatusTrait {
-	typedef T value_type;
-	typedef R return_type
-	typedef U user_type;
-	typedef K key_type;
-	typedef std::function<key_type(const user_type&, const value_type&)> key_op_type;
-	typedef std::function<return_type(const user_type&, const key_type &, const value_type&)> gen_op_type;
-
-	DiffStatusTrait(user_type value, key_op_type fn_key, gen_op_type fn_gen):
-		user(value),
-		key_op(fn_key),
-		gen_op(fn_gen) {
-	}
-
-	const gen_op_type gen_op;
-	const key_op_type key_op;
-	const user_type user;
-}
-
-
-template <typename T>
-struct CalcDiffDistance {
-	typedef std::function<bool(const T&)> both_present_op_type;
-	typedef std::function<bool(const T&)> changed_op_type;
-
-	CalcDiffDistance() {}
-	void operator()(T item) {
-		total++;
-
-		if (is_both_present_op(T)) {
-			total++;
-
-			if (check_changed_op(T))
-				intersect--;
-		} else
-			intersect--;
-
-		distance = (total+intersect) / total;
-	}
-
-	const both_present_op_type both_present_op;
-	const changed_op_type changed_op;
-
-	double distance{0.0};
-	double intersect{0.0};
-	double total{0.0};
-}
-
-
-/*template <typename InputIt1, typename InputIt2, typename OutputIt, typename LessOp, typename DiffOp, typename Wrapper1, typename Wrapper2>
-void diff_status(InputIt1 first1,
-	InputIt1 last1,
-	InputIt2 first2,
-	InputIt2 last2,
-	OutputIt d_first,
-	LessOp less_op,
-	Wrapper1 wrapper1,
-	Wrapper2 wrapper2,
-	LessOp less_op,
-	DiffOp diff_op) {
-
-	while (first1 != last1) {
-		Wrapper1::key_type key1 = wrapper1.key_op(wrapper1.user, *first1);
-
-		if (first2 != last2) {
-			Wrapper2::key_type key2 = wrapper2.key_op(wrapper2.user, *first2);
-
-			if (less_op(key1, key2)) {
-				*d_first = wrapper1.gen_op(wrapper.user, key1, *first1); // present in first1
-				std::advance(first1);
-			} else if (!less_op(key1, key2) && !less_op(key2, key1)) {
-				*d_first = diff_op(key1, key2, *first1, *first2);
-				std::advance(first1);
-				std::advance(first2);
-			} else {
-				*d_first = wrapper2.gen_op(wrapper2.user, key2, *first2); // present in first2
-				std::advance(first2);
-			}
-			
-		} else {
-			*d_first = wrapper1.gen_op(wrapper1.user, key1, *first1); // present in first1
-			std::advance(first1);
-		}
-
-		std::advance(d_first);
-	}
-
-	while (first2 != last2) {
-		Wrapper2::key_type key2 = wrapper2.key_op(wrapper2.user, *first2);
-
-		*d_first = wrapper2.gen_op(wrapper2.user, key2, *first2); // present in first2
-		std::advance(d_first);
-		std::advance(first2);
-	}
-}*/
-
-template <typename InputIt1, typename InputIt2, typename OutputIt, typename LessOp>
+template <typename OutputItValue, typename OutputIt, typename InputIt1,
+	typename InputIt2, typename LessOp>
 double diff_status(InputIt1 first1,
 	InputIt1 last1,
 	InputIt2 first2,
 	InputIt2 last2,
 	OutputIt d_first,
-	std::function<OutputIt::value_type(const InputIt1::value_type&)> gen1_op,
-	std::function<OutputIt::value_type(const InputIt2::value_type&)> gen2_op,
-	std::function<OutputIt::value_type(const InputIt1::value_type&, const InputIt2::value_type&, bool)> gen_both_op,
+	std::function<OutputItValue(const InputIt1::value_type&)> gen1_op,
+	std::function<OutputItValue(const InputIt2::value_type&)> gen2_op,
+	std::function<OutputItValue(const InputIt1::value_type&,
+		const InputIt2::value_type&, bool&)> gen_both_op,
 	LessOp less_op) {
 
 	double total = 0.0;
@@ -118,12 +50,12 @@ double diff_status(InputIt1 first1,
 	while ((first1 != last1) && (first2 != last2)) {
 
 		if (less_op(*first1, *first2)) {
-			*d_first = gen1_op(*first1);
-			std::advance(first1);
+			*d_first++ = gen1_op(*first1);
+			std::advance(first1, 1);
 			intersect--;
 		} else if (!less_op(*first1, *first2) && !less_op(*first2, *first1)) {
 			bool changed = false;
-			*d_first = gen_both_op(*first1, *first2, changed);
+			*d_first++ = gen_both_op(*first1, *first2, changed);
 			std::advance(first1);
 			std::advance(first2);
 			if (changed)
@@ -131,29 +63,26 @@ double diff_status(InputIt1 first1,
 			total++;
 			
 		} else {
-			*d_first = gen2_op(*first2);
+			*d_first++ = gen2_op(*first2);
 			std::advance(first2);
 			intersect--;
 		}
 
-		std::advance(d_first);
 		total++;
 	}
 
 	// copy tail from first1
 	while (first1 != last1) {
-		*d_first = gen1_op(*first1);
+		*d_first++ = gen1_op(*first1);
 		std::advance(first1);
-		std::advance(d_first);
 		total++;
 		intersect--;
 	}
 
 	// copy tail from first2
 	while (first2 != last2) {
-		*d_first = gen2_op(*first2);
+		*d_first++ = gen2_op(*first2);
 		std::advance(first2);
-		std::advance(d_first);
 		total++;
 		intersect--;
 	}
@@ -161,5 +90,6 @@ double diff_status(InputIt1 first1,
 	return (total+intersect) / total;
 }
 
+} // namespace algo
 } // namespace ccu
 #endif
